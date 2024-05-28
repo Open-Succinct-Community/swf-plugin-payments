3f5490e4ca13b1e1f313ea6dc6d7017cd6b7d77a
Venky <venkatramanm@gmail.com>
2024-05-28 16:01:44 +0530

    Refactor

--- src/main/java/com/venky/swf/plugins/payments/db/model/payment/Plan.java
+++ src/main/java/com/venky/swf/plugins/payments/db/model/payment/Plan.java
@@ -1,7 +1,9 @@
 package com.venky.swf.plugins.payments.db.model.payment;
 
 import com.venky.swf.db.annotations.column.COLUMN_DEF;
+import com.venky.swf.db.annotations.column.IS_NULLABLE;
 import com.venky.swf.db.annotations.column.IS_VIRTUAL;
+import com.venky.swf.db.annotations.column.UNIQUE_KEY;
 import com.venky.swf.db.annotations.column.defaulting.StandardDefault;
 import com.venky.swf.db.annotations.model.MENU;
 import com.venky.swf.db.model.Model;
@@ -10,12 +12,26 @@ import com.venky.swf.plugins.collab.db.model.participants.admin.Company;
 
 @MENU("Platform Revenue")
 public interface Plan extends Model {
+    @UNIQUE_KEY
     public String getName();
     public void setName(String name);
 
     @COLUMN_DEF(value = StandardDefault.SOME_VALUE,args = "0")
     public int getNumberOfCredits();
     public void setNumberOfCredits(int numberOfCredits);
+
+    @IS_NULLABLE
+    public Integer  getNumberOfDaysValidity();
+    public void setNumberOfDaysValidity(Integer numberOfDaysValidity);
+
+    @IS_NULLABLE
+    public Integer  getNumberOfMonthsValidity();
+    public void setNumberOfMonthsValidity(Integer numberOfMonthsValidity);
+
+    @IS_NULLABLE
+    public Integer  getNumberOfYearsValidity();
+    public void setNumberOfYearsValidity(Integer numberOfYearsValidity);
+
 
     public int getMaximumRetailPrice();
     public void setMaximumRetailPrice(int maximumRetailPrice);
