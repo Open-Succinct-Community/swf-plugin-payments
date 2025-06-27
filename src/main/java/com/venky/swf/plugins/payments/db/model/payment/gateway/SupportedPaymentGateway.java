2c8df6c216863c69000f5de31471127217d3e629
Venky <venkatramanm@gmail.com>
2025-06-27 18:16:48 +0530

    Unique key added for supported payment gateways

--- src/main/java/com/venky/swf/plugins/payments/db/model/payment/gateway/SupportedPaymentGateway.java
+++ src/main/java/com/venky/swf/plugins/payments/db/model/payment/gateway/SupportedPaymentGateway.java
@@ -1,10 +1,12 @@
 package com.venky.swf.plugins.payments.db.model.payment.gateway;
 
 import com.venky.swf.db.annotations.column.IS_VIRTUAL;
+import com.venky.swf.db.annotations.column.UNIQUE_KEY;
 import com.venky.swf.db.annotations.column.ui.WATERMARK;
 import com.venky.swf.db.model.Model;
 
 public interface SupportedPaymentGateway extends Model {
+    @UNIQUE_KEY
     String getName();
     void setName(String name);
     
